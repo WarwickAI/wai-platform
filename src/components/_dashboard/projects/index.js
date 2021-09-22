de164430071237aca49db9350baaabbc1f034026
Joe Hewett <joehewett1@gmail.com>
2021-09-22 17:35:59 +0100

    Change logo

---
+++ src/components/_dashboard/projects/index.js
@@ -0,0 +1,3 @@
+export { default as ProjectPostCard } from './ProjectPostCard';
+export { default as ProjectPostsSearch } from './ProjectPostsSearch';
+export { default as ProjectPostsSort } from './ProjectPostsSort';
