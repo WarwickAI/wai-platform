cd25dfc39d9d8f6df793b78b34e7962659632921
Edward Upton <ed.upton@outlook.com>
2021-10-18 18:11:13 +0100

    Removed some unecesssary code in pages/Events.js

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,50 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Icon } from '@iconify/react';
-import plusFill from '@iconify/icons-eva/plus-fill';
-import { Link as RouterLink } from 'react-router-dom';
-import { API, Auth } from 'aws-amplify';
 // material
-import { Grid, Button, Container, Stack, Typography } from '@material-ui/core';
+import { Container, Stack, Typography } from '@material-ui/core';
 // components
-import { trackWindowScroll } from 'react-lazy-load-image-component';
 import Page from '../components/Page';
-import CoursePostCard from '../components/_dashboard/courses/CoursePostCard';
-import { listCourses } from '../graphql/queries';
 
 // ----------------------------------------------------------------------
 
-const SORT_OPTIONS = [
-  { value: 'latest', label: 'Latest' },
-  { value: 'popular', label: 'Popular' },
-  { value: 'oldest', label: 'Oldest' }
-];
-
-// ----------------------------------------------------------------------
-
-function Events({ scrollPosition }) {
-  const [courses, setCourses] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  function fetchCourses() {
-    API.graphql({
-      query: listCourses,
-      variables: {
-        limit: 100
-      }
-    })
-      .then((result) => {
-        const loadedCourses = result.data.listCourses.items;
-
-        setCourses(loadedCourses);
-        setIsLoading(false);
-      })
-      .catch(console.log);
-  }
-
-  useEffect(() => {
-    fetchCourses();
-  }, []);
-
+function Events() {
   return (
     <Page title="Events">
       <Container>
@@ -68,4 +30,4 @@ function Events({ scrollPosition }) {
   );
 }
 
-export default trackWindowScroll(Events);
+export default Events;
