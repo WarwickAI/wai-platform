519e92fd6080731837523d2de23b47eab90e347e
Edward Upton <ed.upton@outlook.com>
2021-10-18 20:55:04 +0100

    Added comments and removed unecessary code

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 // material
-import { Container, Stack, Typography, Grid, Item } from '@material-ui/core';
+import { Container, Stack, Typography } from '@material-ui/core';
 // components
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -9,14 +9,15 @@ import Page from '../components/Page';
 
 // ----------------------------------------------------------------------
 
+// ID for WAI public calendar
 const calendarId = '16gnvov94ele73k1e7ekbaqr08@group.calendar.google.com';
+// Used for time localisation.
 const localizer = momentLocalizer(moment);
 
 function Events() {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    console.log(process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY);
     fetch(
       `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${process.env.REACT_APP_GOOGLE_CALENDAR_API_KEY}`
     )
@@ -27,6 +28,7 @@ function Events() {
         } else {
           const resEvents = result.items;
           const rbcEvents = [];
+          // Iterate through events received from google calendar API and format into RBC format.
           resEvents.forEach((event) => {
             const rbcEvent = {
               title: event.summary,
@@ -48,6 +50,7 @@ function Events() {
             Events
           </Typography>
         </Stack>
+        {/* TO-DO: make the height responsive */}
         <div style={{ height: 600 }}>
           <Calendar
             localizer={localizer}
