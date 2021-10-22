16e65a6d3cdffd19fa9c54a94fca0469da648b2f
engiego <ed.upton@outlook.com>
2021-10-22 23:27:16 +0100

    Created test for lambda function at /testLambda

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,11 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const echo = /* GraphQL */ `
+  query Echo($msg: String) {
+    echo(msg: $msg)
+  }
+`;
 export const getTalk = /* GraphQL */ `
   query GetTalk($id: ID!) {
     getTalk(id: $id) {
---
+++ src/pages/TestLambda.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { API, Auth } from 'aws-amplify';
+import * as queries from '../graphql/queries';
+
+function TestLambda() {
+  const [text, setText] = useState('waiting');
+
+  useEffect(() => {
+    API.graphql({ query: queries.echo, variables: { msg: 'Hello there' } }).then((response) =>
+      setText(response.data.echo)
+    );
+  }, []);
+
+  const tryItout = async (e) => {
+    e.preventDefault();
+    const response = await API.graphql({
+      query: queries.echo,
+      variables: { msg: e.target.theText.value }
+    });
+    setText(response.data.echo);
+  };
+
+  return (
+    <div>
+      <form onSubmit={tryItout}>
+        <input name="theText" type="text" />
+        <input type="submit" />
+      </form>
+      <div>{text}</div>
+    </div>
+  );
+}
+
+export default TestLambda;
